8ede34769c99951375932f0dc3453b130a7651d9
Carlos Sanchez <u20201c222@upc.edu.pe>
2023-06-02 18:08:10 -0500

    Cambio de Nombre a carpetas de Tests

--- src/test/java/pe/com/betweenAll/BetweenAllApplicationTests.java
+++ src/test/java/pe/com/betweenAll/BetweenAllApplicationTests.java
@@ -1,10 +1,10 @@
-package pe.com.petstore.backend;
+package pe.com.betweenAll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BetweenAllApplicationTests {
 
 	@Test
 	void contextLoads() {
